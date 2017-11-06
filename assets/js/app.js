7107aefa7e7eae5e680f741ea57781ac2400b2fb
verocortesferrera <verocortesferrera@gmail.com>
2017-11-06 09:53:29 -0300

    arreglo js, funciona input

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,10 +12,10 @@ boton.addEventListener("click", function(){
 	var post = document.getElementById("posteo");
 
 	// div que contiene cada post se ira a una caja
-	var newPost = document.createElement('div');
+	var newPost = document.createElement("div");
 
 	//validar que textarea tenga un msje
-	if(post.length == 0 || post == null) { // si no hay nada
+	if(comment.length == 0 || comment == null) { // si no hay nada
 		alert('Debes ingresar un mensaje');
 		return false;// para que se corte ahi la condicion sino sera true y se crea la condicion
 	};
@@ -24,7 +24,7 @@ boton.addEventListener("click", function(){
 	var textNewPost = document.createTextNode(texto);
 
 	// guardo el texto en un elemento p
-	var contenedorElemento = document.createElement('p');
+	var contenedorElemento = document.createElement("p");
 
 	//uno el elemento p con el nodo de texto ingresado
 	contenedorElemento.appendChild(textNewPost);
@@ -38,33 +38,20 @@ boton.addEventListener("click", function(){
 
 }, false);
 
-//contador de caracteres
-var contenidoTextarea = "";
-//permitidos
-var numCaracterPermitidos = 140;
 
-function(){ 
-   //variable de conteo de caracteres ingresados
-	var textoIngresado = document.getElementById("comment").value.length;
-	//llamo al contenedor donde ira el conteo
-	var caracter= document.getElementById("caracteres");
-
-	if( textoIngresado > numCaracterPermitidos){
-		document.getElementById("comment").value = contenidoTextarea;
-	}
-/*
-// creo un nodo texto ingresado 
-var numero = document.createTextNode(textoIngresado);
-
-//un elemento p que agarre el numero
-var contenedorNumero = document.createElement('p');
-
-//uno el p con nodo ingresado
-contenedorNumero.appendChild(numero);
-//unir el nodo con el div del html
-caracter.appendChild(contenedorNumero)*/
-
-caracter = textoIngresado;
+//calcular caracteres
 
 
-} 
+
+ comment.onkeyup = function(){
+
+ 	var limiteInicial = document.getElementById("limite");
+ 	
+	var comment = document.getElementById("comment");
+ 	//caracteres del input
+ 	var caracter = comment.value.length;
+
+
+
+
+ }
